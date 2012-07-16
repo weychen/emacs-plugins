29ccb4915f7314f00bbd587fcae2e5a17dd6a225
Menglong Tan <tanmenglong@gmail.com>
2012-07-16 09:30:53 +0800

    u

--- workmodes/markdown/init.el
+++ workmodes/markdown/init.el
@@ -2,6 +2,6 @@
 (autoload 'markdown-mode "markdown-mode.el"
     "Major mode for editing Markdown files" t)
 (setq auto-mode-alist
-    (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
+    (cons '("\\.md" . markdown-mode) auto-mode-alist))
 
 (load-file "~/.emacs.d/emacs-plugins/workmodes/markdown/keybindings.el")
