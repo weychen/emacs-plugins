97a0dda939e122c916e0b2eed4b8ae6939657285
Menglong Tan <tanmenglong@gmail.com>
2012-12-15 20:46:01 +0800

    u

--- workmodes/cpp/load-codingstyle.el
+++ workmodes/cpp/load-codingstyle.el
@@ -1,8 +1,8 @@
 (add-to-list 'load-path "~/.emacs.d/emacs-plugins/workmodes/cpp")
 
 ;; crackcell coding style
-(require 'crackcell-c-style)
-(crackcell-set-c-style)
-(crackcell-make-newline-indent)
-;(add-hook 'c-mode-common-hook 'crackcell-set-c-style)
-;(add-hook 'c-mode-common-hook 'crackcell-make-newline-indent)
+(require 'ecom-c-style)
+(ecom-set-c-style)
+(ecom-make-newline-indent)
+(add-hook 'c-mode-common-hook 'ecom-set-c-style)
+(add-hook 'c-mode-common-hook 'ecom-make-newline-indent)
