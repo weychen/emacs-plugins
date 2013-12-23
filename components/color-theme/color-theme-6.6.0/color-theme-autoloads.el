553f3e46c7386a6ac85521b566ef8a45c468a526
Menglong Tan <tanmenglong@gmail.com>
2013-12-23 23:17:13 +0800

    up

--- components/color-theme/color-theme-6.6.0/color-theme-autoloads.el
+++ components/color-theme/color-theme-6.6.0/color-theme-autoloads.el
@@ -15,7 +15,7 @@
 ;;; Generated autoloads follow (made by autoload.el).
 
 ;;;### (autoloads nil nil ("themes/color-theme-example.el" "themes/color-theme-library.el")
-;;;;;;  (20964 34471 614583))
+;;;;;;  (21174 65069 522640 0))
 
 ;;;***
 
@@ -23,7 +23,7 @@
 ;;;;;;  color-theme-compare color-theme-make-snapshot color-theme-analyze-defun
 ;;;;;;  color-theme-print color-theme-install-at-point-for-current-frame
 ;;;;;;  color-theme-install-at-mouse color-theme-describe color-theme-select)
-;;;;;;  "color-theme" "color-theme.el" (20964 7930))
+;;;;;;  "color-theme" "color-theme.el" (21174 64966 0 0))
 ;;; Generated autoloads from color-theme.el
 
 (autoload 'color-theme-select "color-theme" "\
