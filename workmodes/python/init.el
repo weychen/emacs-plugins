a2d865eabd84f2b490df265e2893f1a657693798
Menglong Tan <tanmenglong@gmail.com>
2014-01-26 22:16:18 +0800

    update home folder

--- workmodes/python/init.el
+++ workmodes/python/init.el
@@ -4,13 +4,12 @@
 (setq whitespace-style '(face empty tabs lines-tail trailing))
 (global-whitespace-mode t)
 
-
-(add-to-list 'load-path "/home/crackcell/.emacs.d/emacs-plugins/workmodes/python/emacs-deferred")
-(add-to-list 'load-path "/home/crackcell/.emacs.d/emacs-plugins/workmodes/python/emacs-ctable")
-(add-to-list 'load-path "/home/crackcell/.emacs.d/emacs-plugins/workmodes/python/emacs-epc")
+(add-to-list 'load-path "~/.emacs.d/emacs-plugins/workmodes/python/emacs-deferred")
+(add-to-list 'load-path "~/.emacs.d/emacs-plugins/workmodes/python/emacs-ctable")
+(add-to-list 'load-path "~/.emacs.d/emacs-plugins/workmodes/python/emacs-epc")
 (require 'epc)
 
-(add-to-list 'load-path "/home/crackcell/.emacs.d/emacs-plugins/workmodes/python/emacs-jedi")
+(add-to-list 'load-path "~/.emacs.d/emacs-plugins/workmodes/python/emacs-jedi")
 (autoload 'jedi:setup "jedi" nil t)
 
 (setq jedi:setup-keys t)
