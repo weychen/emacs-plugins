058d729b986b0302f7a932c2f607077e834a59b7
Menglong Tan <tanmenglong@gmail.com>
2014-06-12 12:12:38 +0800

    up

--- workmodes/go/init.el
+++ workmodes/go/init.el
@@ -1,13 +1,17 @@
 (add-to-list 'load-path "~/.emacs.d/emacs-plugins/workmodes/go")
-(add-to-list 'load-path "~/.emacs.d/emacs-plugins/components/go-mode")
 
 ;; 设置缩进风格
 ;(customize-variable (quote tab-stop-list))
-(setq-default indent-tabs-mode t)
-(setq indent-tabs-mode t)
-(setq default-tab-width 2)
-(setq tab-width 2)
-(setq tab-stop-list ())
+;(setq-default indent-tabs-mode t)
+;(setq indent-tabs-mode t)
+;(setq default-tab-width 2)
+;(setq tab-width 2)
+;(setq tab-stop-list ())
+
+;(load-file "~/.emacs.d/emacs-plugins/workmodes/go/go-mode.el")
+;(load-file "~/.emacs.d/emacs-plugins/workmodes/go/go-mode-load.el")
+;(load-file "~/.emacs.d/emacs-plugins/workmodes/go/go-autocomplete.el")
+;(load-file "~/.emacs.d/emacs-plugins/workmodes/common/load-auto-complete.el")
 
 (require 'go-mode-load)
 (require 'go-autocomplete)
@@ -15,9 +19,11 @@
 
 (add-hook 'before-save-hook 'gofmt-before-save)
 
+; remove unused imports
+(add-hook 'go-mode-hook (lambda ()
+                          (global-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
+
 ;; 改变每行超出80个字符部分的颜色
 (require 'whitespace)
 (setq whitespace-style '(face empty tabs lines-tail trailing))
 (global-whitespace-mode t)
-
-;(load-file "~/.emacs.d/emacs-plugins/workmodes/go/shortcuts.el")
