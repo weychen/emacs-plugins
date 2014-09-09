65e986889846a3031f12ffe7a10e9a5c53c97450
Menglong Tan <tanmenglong@gmail.com>
2014-09-09 14:37:45 +0800

    up

--- scripts/init-shell.sh
+++ scripts/init-shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 alias emacs='emacs -nw'
-alias eo='emacs -l ~/.emacs.d/emacs-plugins/workmodes/org/init.el'
-alias ediff='emacs -l ~/.emacs.d/emacs-plugins/workmodes/ediff/init.el'
-alias ess='emacs -l ~/.emacs.d/emacs-plugins/workmodes/r/init.el'
+alias eo='emacs -l ~/.emacs.d/emacs-plugins/bundles/org/init.el'
+alias ediff='emacs -l ~/.emacs.d/emacs-plugins/bundles/ediff/init.el'
+alias ess='emacs -l ~/.emacs.d/emacs-plugins/bundles/r/init.el'
