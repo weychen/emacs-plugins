4f45a03acd5f32bc738b79c19ffe7f4ddcf47e38
Menglong Tan <tanmenglong@gmail.com>
2014-11-26 14:07:55 +0800

    up

--- bundles/markdown/init.el
+++ bundles/markdown/init.el
@@ -1,7 +1,10 @@
 (add-to-list 'load-path "~/.emacs.d/emacs-plugins/bundles/markdown/")
-(autoload 'markdown-mode "markdown-mode.el"
-    "Major mode for editing Markdown files" t)
-(setq auto-mode-alist
-    (cons '("\\.md" . markdown-mode) auto-mode-alist))
+
+(load-file "~/.emacs.d/emacs-plugins/bundles/markdown/markdown-mode.el")
+
+(autoload 'markdown-mode "markdown-mode"
+	"Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 (load-file "~/.emacs.d/emacs-plugins/bundles/markdown/keybindings.el")
+
